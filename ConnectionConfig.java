accfae98a7d5c3e9726f3c0a97ad251f0d373acf
Scott Weaver <sweaver69@gmail.com>
2015-11-07 11:20:28 -0600

    SEM 0.6
    Upload of files.

---
+++ ConnectionConfig.java
@@ -0,0 +1,27 @@
+package pkg430test;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionConfig
+{
+
+    public static Connection getConnection()
+    {
+        Connection connection = null;
+        String URL = "jdbc:oracle:thin:@xxx";
+        String USER = "xxx";
+        String PASS = "xxx";
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+}
